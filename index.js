9190bf7c1053b58eb1cbf31b5236ec3c8ea39db2
Luciano Menezes <lucianomenezes655@gmail.com>
2024-01-28 20:54:26 -0300

    correção de erro

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ const ValidateCreditCard = require('./utils/ValidateCreditCard')
 const ValidatePassword = require('./utils/ValidatePassword')
 const ValidateCNPJ = require('./utils/ValidateCNPJ')
 const ValidateCEP = require('./utils/ValidateCEP')
+const FormatDate = require('./utils/FormatDate')
 
 module.exports = {
     FormatString,
+    FormatDate,
     RemoveCurseWords,
+    ParseURL,
     ValidateCPF,
     ValidatePhoneNumber,
     ValidateEmail,
-    ParseURL,
     ValidateCreditCard,
     ValidatePassword,
     ValidateCNPJ,
